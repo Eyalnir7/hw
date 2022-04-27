3d313537f90d377c4193e819fa394f11b2ec0e10
lion12304 <53821960+lion12304@users.noreply.github.com>
2022-04-27 22:33:42 +0300

    hummus again

--- src/SnakesAndLaddersGame.java
+++ src/SnakesAndLaddersGame.java
@@ -34,6 +34,7 @@ public class SnakesAndLaddersGame {
             splitInput = input.split(" ");
             handleInput(splitInput);
         }
+        sortPlayers(players, 0 , players.length - 1);
     }
 
     //Handles the given input from the user about the game
@@ -110,6 +111,62 @@ public class SnakesAndLaddersGame {
         return false;
     }
 
+    void merge(Player arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        /* Create temp arrays */
+        Player L[] = new Player[n1];
+        Player R[] = new Player[n2];
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+        /* Merge the temp arrays */
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i].getName().compareTo(R[j].getName()) > 0) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    //Sorts the players array alphabetically
+    void sortPlayers(Player arr[], int l, int r)
+    {
+        if (l < r) {
+            // Find the middle point
+            int m =l+ (r-l)/2;
+            // Sort first and second halves
+            sortPlayers(arr, l, m);
+            sortPlayers(arr, m + 1, r);
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
 
     //Starts the game and plays through it
     public String start()
